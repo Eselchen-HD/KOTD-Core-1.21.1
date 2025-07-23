a30159a241778f05df48f9ebc6940a49090e8df7
Eselchen <michaelkehm1@gmail.com>
2025-07-24 01:22:42 +0200

    KOTD-Core Config Menu added for the KOTD Armor

--- src/main/java/de/eselgamerhd/kotd/common/init/IEnergyContainer.java
+++
@@ -1,8 +0,0 @@
-package de.eselgamerhd.kotd.common.init;
-
-public interface IEnergyContainer {
-    int receiveEnergy(int maxReceive, boolean simulate);
-    int extractEnergy(int maxExtract, boolean simulate);
-    int getEnergyStored();
-    int getMaxEnergyStored();
-}
